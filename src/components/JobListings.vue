4154eabd50b1feeca667dabf1b54afe22e84b094
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2026-02-05 13:50:18 +0200

    finish the tutorial

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -52,9 +52,7 @@ onMounted(async () => {
 
             <!-- Show jhob listing when done loading -->
             <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job">
-                    {{ job.title }}
-                </JobListing>
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
